70828a5e85da495d45cf58bb4486df5df8076ad9
CristelRR <cristel23rr@gmail.com>
2023-06-17 15:30:20 -0600

    Versión 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
+import { InstitucionalComponent } from './componentes/institucional/institucional.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    InstitucionalComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
